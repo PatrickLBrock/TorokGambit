91a52d0b7441ba52e72f1c8ebdc722866289f3d8
PatrickLBrock <patrickleonard0312@gmail.com>
2023-01-13 01:13:16 -0500

    basic movement and chessboard

--- Toroks gambit/Assets/Scripts/Board.cs
+++ Toroks gambit/Assets/Scripts/Board.cs
@@ -9,10 +9,25 @@ public class Board : MonoBehaviour
 
     private GameObject[,] chessBoard;
 
+    [SerializeField]
+    private GameObject boardSquare;
+
+    [SerializeField]
+    private GameObject chessPiece;
+
+    [SerializeField]
+    private Camera camera;
+
+    private GameObject storedPiece;
+
     // Start is called before the first frame update
     void Start()
     {
         chessBoard = new GameObject[boardSize,boardSize];
+
+        Debug.Log(transform.position);
+
+        BuildBoard();
 
         
     }
@@ -20,11 +35,43 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        if (Physics.Raycast(ray, out hit)) 
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            if(hit.transform.tag == "Chess Piece")
+            {
+                storedPiece = hit.transform.gameObject;
+            }
+            if(hit.transform.tag == "Chess Board" && storedPiece)
+            {
+                storedPiece.transform.position = hit.transform.position + new Vector3(0,1,0);
+
+            }
+        }
+        }
+
     }
 
     private void BuildBoard()
     {
+        for(int i=0;i<boardSize;i++)
+        {
+            for(int j=0;j<boardSize;j++)
+            {
+                GameObject instance = Instantiate(boardSquare,(transform.position + new Vector3(i,0,j)), Quaternion.identity);
+                instance.gameObject.name = i + "_" + j; 
+            }
+        }
+
+            GameObject pieceInstance = Instantiate(chessPiece,(transform.position + new Vector3(0,1,3)), Quaternion.identity);
+            pieceInstance.gameObject.name = "chessPiece"; 
+
         
     }
 }
